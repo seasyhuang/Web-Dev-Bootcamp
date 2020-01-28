cdfa0420ab6693399907e9821ddf966034cafad5
Seasy <seasy.huang@gmail.com>
2020-01-28 12:11:47 -0500

    js

---
+++ agecalculator.js
@@ -0,0 +1,3 @@
+var age = prompt("what is your age?")
+var days = 365.25 * age
+alert(age + " years is roughly " + days + " days.")
---
+++ script.js
@@ -0,0 +1,3 @@
+var username = prompt("Name?")
+console.log(username)
+alert("hi " + username)
